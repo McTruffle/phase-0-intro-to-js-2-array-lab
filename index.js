c9b6ff39b2ae00a35a59389a45cc41bffa85c303
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-05-25 02:31:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const cats = (["Milo", "Otis", "Garfield"]);
+
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+
+function appendCat(name) {  
+    let cats2 = (["Milo", "Otis", "Garfield"]);
+    cats2.push(name);
+    return (cats2);
+};
+
+
+function prependCat(name) {
+    let cats2 = (["Milo", "Otis", "Garfield"]);
+    cats2.unshift(name);
+    return (cats2);
+};
+
+function removeLastCat() {
+    let cats2 = (["Milo", "Otis", "Garfield"]);
+    cats2.pop();
+    return (cats2);
+};
+
+function removeFirstCat() {
+    let cats2 = (["Milo", "Otis", "Garfield"]);
+    cats2.shift();
+    return (cats2);
+};
